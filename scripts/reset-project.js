42fbb84b6be24dc42706c4c4d7621212cfe0d532
lily <lilyuzmi@gmail.com>
2024-09-03 16:10:29 -0300

    gay

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -1,11 +1,21 @@
+#!/usr/bin/env node
+
+/**
+ * This script is used to reset the project to a blank state.
+ * It moves the /app directory to /app-example and creates a new /app directory with an index.tsx and _layout.tsx file.
+ * You can remove the `reset-project` script from package.json and safely delete this file after running it.
+ */
 
 const fs = require('fs');
 const path = require('path');
+
 const root = process.cwd();
 const oldDirPath = path.join(root, 'app');
 const newDirPath = path.join(root, 'app-example');
 const newAppDirPath = path.join(root, 'app');
+
 const indexContent = `import { Text, View } from "react-native";
+
 export default function Index() {
   return (
     <View
@@ -20,6 +30,7 @@ export default function Index() {
   );
 }
 `;
+
 const layoutContent = `import { Stack } from "expo-router";
 
 export default function RootLayout() {
@@ -30,6 +41,7 @@ export default function RootLayout() {
   );
 }
 `;
+
 fs.rename(oldDirPath, newDirPath, (error) => {
   if (error) {
     return console.error(`Error renaming directory: ${error}`);
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react';
 import { Surface, Text } from 'react-native-paper';
-import styles from '../config/styles';
+import styles from '../config/style';
 import TrocaTema from './TrocaTema';
-import { TemaContext } from '../contexts/TemaContext';
+import { TemaContext } from '../context/TemaContext';
 
 export default function HomeScreen() {
-const { tema } = useContext(TemaContext);
-return (
+  const { tema } = useContext(TemaContext);
+
+  return (
     <Surface style={styles.container}>
-      <Text>cavalos homosexuais</Text>
+      <Text>Clique abaixo para trocar o tema</Text>
       <TrocaTema />
     </Surface>
   );
